d7a10a56531b9c3e1c27c437b096077472b42d88
Rushana Sabyrzhanovagit config --global user.email rushanasabyrzhanova@gmail.com <rushanasabyrzhanova@gmail.com>
2024-12-27 20:39:11 +0300

    create folder and vednak

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Ведьмаку заплатите чеканой монетой!</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore,
+        officiis blanditiis facilis facere dolorum aspernatur? Doloribus,
+        officia eius! Autem doloribus sunt et reiciendis! Ipsa, culpa? Fugit
+        cupiditate assumenda dolores deleniti eaque pariatur sapiente deserunt
+        dolore?
+      </p>
+      <iframe
+        width="720"
+        height="405"
+        src="https://rutube.ru/video/52475dd9e9e9a029fcb9ead24024e861/?r=wd"
+        frameborder="0"
+        allow=""
+      ></iframe>
+    </div>
+  );
+};
+
+export default App;
